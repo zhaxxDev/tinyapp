3f00f7adeaa5bbfd89079e58373ee13beba8799e
Z Xian <alex.xian@hotmail.com>
2020-09-29 18:44:40 +0000

    cool

--- express_server.js
+++ express_server.js
@@ -1,8 +1,20 @@
 const express = require("express");
 const app = express();
 const PORT = 8080;
-
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
+
+function generateRandomString() {
+  var result           = '';
+   var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+   var charactersLength = characters.length;
+   for ( var i = 0; i < 6; i++ ) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+   }
+   return result;
+}
+
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -30,6 +42,10 @@ app.get("/set", (req, res) => {
   res.send(`a = ${a}`);
  });
  
+ app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
  app.get("/fetch", (req, res) => {
   res.send(`a = ${a}`);
  });
@@ -42,4 +58,9 @@ app.get("/set", (req, res) => {
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: req.params.longURL };
   res.render("urls_show", templateVars);
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
 });
